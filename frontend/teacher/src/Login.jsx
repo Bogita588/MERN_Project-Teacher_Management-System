caa741cea8a7e0c827bb812a51a47b78614a3508
Bogita Brian <bogitabrian@gmail.com>
2023-09-29 16:26:22 +0300

    Added server file and connected it to mysql database

--- frontend/teacher/src/Login.jsx
+++ frontend/teacher/src/Login.jsx
@@ -1,24 +1,38 @@
-import React from 'react'
+import { useState } from 'react'
+import './assets/login.css'
+import axios from'axios'
 
 const Login = () => {
+    const [values, setValues] =useState({
+        email: '',
+        password: '',
+    })
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post('http://localhost:8080/login', values)
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+
+    }
   return (
     
-    <div className='d-flex justify-content-center align-items-center vh-100 loginPage'>
+    <div className='d-flex justify-content-center align-items-center vh-100 backImage'>
             <div className='p-3 rounded w-25 border loginForm'>
                 <div className='text-danger'>
                     
                 </div>
                 <h2>Login</h2>
-                <form >
+                <form onSubmit={handleSubmit} >
                     <div className='mb-3'>
                         <label htmlFor="email"><strong>Email</strong></label>
                         <input type="email" placeholder='Enter Email' name='email' 
-                          className='form-control rounded-0' autoComplete='off'/>
+                         onChange={e => setValues({...values, email: e.target.value})} className='form-control rounded-0' autoComplete='off'/>
                     </div>
                     <div className='mb-3'>
                         <label htmlFor="password"><strong>Password</strong></label>
                         <input type="password" placeholder='Enter Password' name='password'
-                           className='form-control rounded-0' />
+                         onChange={e => setValues({...values, password: e.target.value})}  className='form-control rounded-0' />
                     </div>
                     <button type='submit' className='btn btn-success w-100 rounded-0'> Log in</button>
                     <p>You are agree to aour terms and policies</p>
