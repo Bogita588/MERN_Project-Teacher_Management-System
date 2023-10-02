4b39d7ff312e827341c12bfd7e539cb9be03f9a5
Bogita Brian <bogitabrian@gmail.com>
2023-10-02 15:28:01 +0300

    Added code

--- server/server.js
+++ server/server.js
@@ -2,8 +2,10 @@ import express from 'express'
 import mysql from 'mysql'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
+import multer from 'multer'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import path from 'path'
 
 
 const app = express();
@@ -29,17 +31,38 @@ con.connect(function(err) {
     }
 })
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname));
+    }
+})
+
+const upload = multer({
+    storage: storage
+})
+
 app.post('/login', (req, res) => {
     const sql ="SELECT * FROM users Where email = ? AND password = ?";
     con.query(sql, [req.body.email, req.body.password], (err, result) =>{
-        if(err) return res.json({Status: "Error occured"});
+        if(err) return res.json({Status: "Error occured", Error: 'Error running query'});
         if(result.length > 0){
             return res.json({Status: "Success"})
         } else {
-            return res.json({Status: "Error"})
+            return res.json({Status: "Error", Error: "Wrong Email or password"})
         }
     })
 })
+
+
+app.post('/create',upload.single('image'), (req, res) => { 
+    console.log(req.file);
+})
+
+
+
 app.listen(8080, ()=> {
     console.log("Running");
 })
